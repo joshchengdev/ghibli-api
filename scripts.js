13dfc514df29054982d3407193089b10e1b51ec2
Josh <jcheng115@gmail.com>
2019-01-10 00:43:54 -0800

    Added script

---
+++ scripts.js
@@ -0,0 +1,55 @@
+const app = document.getElementById("root");
+
+const logo = document.createElement("img");
+logo.src = "logo.png";
+
+const container = document.createElement("div");
+container.setAttribute("class", "container");
+
+app.appendChild(logo);
+app.appendChild(container);
+
+// Create a request variable and assign a new XMLHttpRequest object to it
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
+
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      //   console.log(movie.title);
+      // console.log(movie.director);
+      console.log(movie.rt_score);
+      const card = document.createElement("div");
+      card.setAttribute("class", "card");
+
+      const h1 = document.createElement("h1");
+      h1.textContent = movie.title;
+
+      const h2 = document.createElement("h2");
+      h2.textContent = movie.director;
+
+      const p = document.createElement("p");
+      movie.description = movie.description.substring(0, 300); //limit to 300 characters
+      p.textContent = `${movie.description}...`;
+
+      const score = document.createElement("span");
+      score.textContent = `Rotten Tomato Score: ${movie.rt_score}`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(h2);
+      card.appendChild(p);
+      card.appendChild(score);
+    });
+  } else {
+    const errorMessage = document.createElement("marquee");
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
+};
+// Send request
+request.send();
